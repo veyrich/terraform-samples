d4686cad59b74ebeebb7a534416aa0269868710d
veyrich <veyrich@>
2023-07-13 03:41:30 +0000

    address API dependencies; always enable cloud resource manager

--- proj_setup/main.tf
+++ proj_setup/main.tf
@@ -3,9 +3,9 @@
 terraform {
   required_version = ">= 1.5.2"
   required_providers {
-  google = {
-    source = "hashicorp/google"
-    version = ">= 4.72.0"
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.72.0"
     }
   }
 }
@@ -14,25 +14,24 @@ provider "google" {
   project = var.project_id
   region  = var.gcp_region
 }
-
 #enable required APIs
 module "apis" {
-       source = "./modules/apis"
-       apis = ["compute.googleapis.com",
-       	       "cloudresourcemanager.googleapis.com"]
-       project_id = var.project_id
+  source = "./modules/apis"
+  apis = ["compute.googleapis.com",
+  "orgpolicy.googleapis.com"]
+  project_id = var.project_id
 }
 
 #create default VPC
 module "vpc" {
-       source = "./modules/vpc"
-       #explicit dependcy on Compute Engine API
-       depends_on = [ module.apis ]
+  source = "./modules/vpc"
+  #explicit dependcy on Compute Engine API
+  depends_on = [module.apis]
 }
 
 #NAT router / gateway
 module "nat" {
-       source = "./modules/nat"
-       network_name = "default"
-       depends_on = [ module.vpc ]
+  source       = "./modules/nat"
+  network_name = "default"
+  depends_on   = [module.vpc]
 }
--- proj_setup/modules/apis/main.tf
+++ proj_setup/modules/apis/main.tf
@@ -1,6 +1,13 @@
 #enable APIs
+#cloudresourcemanager is enabled by default  
+locals {
+       required_apis = [ "cloudresourcemanager.googleapis.com" ]
+       #APIs requested by the user
+       all_apis = concat(local.required_apis,var.apis)
+}
+
 resource "google_project_service" "api" {
-  count = length(var.apis)
-  service = var.apis[count.index]
+  count = length(local.all_apis)
+  service = local.all_apis[count.index]
   disable_dependent_services=true
 }
